8dbc0625767043b563a1d2aaa2f4055b7546e25d
Rinaldo <rrossi@byte-code.com>
2017-01-19 23:55:30 +0100

    forbid redux dev tool in production

--- src/store.js
+++ src/store.js
@@ -1,14 +1,19 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
 
+const composeEnhancers =
+    process.env.NODE_ENV !== 'production' &&
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify here name, actionsBlacklist, actionsCreators and other options
+      }) : compose;
+
 export const getStore = () => {
 
     const initialState = {},
           middleware = [],
-          enhancers = [
-              applyMiddleware(...middleware),
-              window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-              ];
+          enhancers = composeEnhancers(applyMiddleware(...middleware));
 
     return createStore(reducers, initialState, compose(...enhancers));
 };
